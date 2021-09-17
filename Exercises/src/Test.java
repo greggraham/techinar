12dd98e1a2b3fc3e99f34e930a13017b83600f1e
Greg Graham <greg@grahamtx.net>
2021-09-17 13:56:39 -0500

    Example 2

--- Exercises/src/Test.java
+++ Exercises/src/Test.java
@@ -1,14 +1,14 @@
 public class Test {
-    int a = 4;
+    int[] array = new int[] {1, 2, 3};
 
-    static void increaseInt(int anInt) {
-        anInt++;
+    static void increaseInt(int[] anIntArray) {
+        anIntArray[0]++;
     }
 
     public static void main(String[] args) {
         Test t = new Test();
-        System.out.println("Instance var a is: " + t.a);
-        Test.increaseInt(t.a);
-        System.out.println("Instance var a is now: " + t.a);
+        System.out.println("Instance var a is: " + t.array[0]);
+        Test.increaseInt(t.array);
+        System.out.println("Instance var a is now: " + t.array[0]);
     }
 }
